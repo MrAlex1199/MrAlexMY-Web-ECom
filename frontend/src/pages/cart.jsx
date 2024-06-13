99735e88f42daa9a709476b8292e5e4d90e33162
Mrboss1199 <t57havytanks@gmail.com>
2024-06-13 14:55:25 +0700

    EditDeliveryPrice

--- frontend/src/pages/cart.jsx
+++ frontend/src/pages/cart.jsx
@@ -170,44 +170,11 @@ return (
                   <div className="flex pb-6">
                     <div className="relative w-full">
                       <div className=" absolute left-0 top-0 py-3 px-4">
-                        <span className="font-normal text-base text-gray-300">Second Delivery</span>
+                        <span className="font-normal text-base text-gray-300">Delivery Price</span>
                       </div>
                       <input type="text"
                       className="block w-full h-11 pr-10 pl-36 min-[500px]:pl-52 py-2.5 text-base font-normal shadow-xs text-gray-900 bg-white border border-gray-300 rounded-lg placeholder-gray-400 focus:outline-gray-400"
-                      placeholder="$5.00"/>
-                      <button id="dropdown-button" data-target="dropdown-delivery"
-                      className="dropdown-toggle flex-shrink-0 z-10 inline-flex items-center py-4 px-4 text-base font-medium text-center text-gray-900 bg-transparent  absolute right-0 top-0 pl-2 "
-                      type="button">
-                      <svg className="ml-2 my-auto" width="12" height="7" viewBox="0 0 12 7"
-                      fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path
-                      d="M1 1.5L4.58578 5.08578C5.25245 5.75245 5.58579 6.08579 6 6.08579C6.41421 6.08579 6.74755 5.75245 7.41421 5.08579L11 1.5"
-                      stroke="#6B7280" strokeWidth="1.5" strokeiinecap="round"
-                      strokeLinejoin="round">
-                      </path>
-                      </svg>
-                      </button>
-                      <div id="dropdown-delivery" aria-labelledby="dropdown-delivery"
-                        className="z-20 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 absolute top-10 bg-white right-0">
-                        <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
-                          <li>
-                              <a href="/"
-                                  className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Shopping</a>
-                          </li>
-                          <li>
-                              <a href="/"
-                                  className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Images</a>
-                          </li>
-                          <li>
-                              <a href="/"
-                                  className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">News</a>
-                          </li>
-                          <li>
-                              <a href="/"
-                                  className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Finance</a>
-                          </li>
-                        </ul>
-                      </div>
+                      placeholder="$5.00" readOnly/>
                     </div>
                   </div>
                   <label className="flex items-center mb-1.5 text-gray-400 text-sm font-medium">
@@ -219,35 +186,6 @@ return (
                       <input type="text"
                         className="block w-full h-11 pr-11 pl-5 py-2.5 text-base font-normal shadow-xs text-gray-900 bg-white border border-gray-300 rounded-lg placeholder-gray-500 focus:outline-gray-400 "
                         placeholder="xxxx xxxx xxxx" />
-                      <button id="dropdown-button" data-target="dropdown"
-                        className="dropdown-toggle flex-shrink-0 z-10 inline-flex items-center py-4 px-4 text-base font-medium text-center text-gray-900 bg-transparent  absolute right-0 top-0 pl-2 "
-                        type="button"><svg className="ml-2 my-auto" width="12" height="7" viewBox="0 0 12 7"
-                          fill="none" xmlns="http://www.w3.org/2000/svg">
-                          <path
-                            d="M1 1.5L4.58578 5.08578C5.25245 5.75245 5.58579 6.08579 6 6.08579C6.41421 6.08579 6.74755 5.75245 7.41421 5.08579L11 1.5"
-                            stroke="#6B7280" strokeWidth="1.5" strokeiinecap="round"
-                            strokeLinejoin="round">
-                          </path>
-                        </svg>
-                      </button>
-                      <div id="dropdown"
-                        className="absolute top-10 right-0 z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                          <ul className="py-2 text-sm text-gray-700 dark:text-gray-200"
-                            aria-labelledby="dropdown-button">
-                            <li>
-                              <a href="/" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Shopping</a>
-                            </li>
-                            <li>
-                              <a href="/" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Images</a>
-                            </li>
-                            <li>
-                              <a href="/" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">News</a>
-                            </li>
-                            <li>
-                              <a href="/" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Finance</a>
-                            </li>
-                          </ul>
-                      </div>
                     </div>
                   </div>
                   <div className="flex items-center border-b border-gray-200">
